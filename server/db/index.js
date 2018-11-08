817936f5be5180c74dd6021d7116b1cea423675d
Kyle Krupp <kyle.krupp@gmail.com>
2018-11-07 21:18:46 -0500

    update mongo urls for heroku

--- server/db/index.js
+++ server/db/index.js
@@ -8,7 +8,7 @@ const MONGO_LOCAL_URL = 'mongodb://localhost/mern-passport'
 
 if (process.env.MONGODB_URI) {
 	mongoose.connect(process.env.MONGODB_URI)
-	MONGO_URL = 'mongodb://root:root@localhost/admin?authSource=admin'
+	MONGO_URL = 'mongodb://heroku_mtln19hq:heroku_mtln19hq"@ds155213.mlab.com:55213/heroku_mtln19hq'
 } else {
 	mongoose.connect('mongodb://root:root@localhost/admin?authSource=admin') // local mongo url
 	MONGO_URL = MONGO_LOCAL_URL
