87094a6b71f8c7fe4e5758aa8df3444bf1f8306e
Andrew Harding <dreweharding@gmail.com>
2018-11-17 12:49:52 -0500

    added conditionals to map functions

--- src/components/List/List.js
+++ src/components/List/List.js
@@ -8,25 +8,29 @@ import NewNote from "../NewNote";
 const List = props => {
     return (
         <div>
-            <Collapsible popout>
-                {props.user.list.map(item => {
-                    return (
-                        <CollapsibleItem key={item.city} header={item.city} icon={item.visited ? 'check_box' : 'check_box_outline_blank'}>
-                            <h5>Here's some info on {item.city}!</h5>
-                            {item.visited ? `You have visited ${item.city}!` : `You have not visited ${item.city} yet!`}
-                            <ul>
-                                <li>Weather:</li>
-                                <li>Current News:</li>
-                            </ul>
-                            {item.notes.map(note => {
-                                return (<p key={note}>{note}</p>)
-                            })}
-                            <NewNote />
-                            <Button waves='light' onClick={() => console.log('remove city')}>Remove City<Icon right>delete</Icon></Button>
-                        </CollapsibleItem>
-                    );
-                })}
-            </Collapsible>
+            {props.user.list.length == 0 ? `You do not currently have any cities on your list.` :
+                <Collapsible popout>
+                    {props.user.list.map(item => {
+                        return (
+                            <CollapsibleItem key={item.city} header={item.city} icon={item.visited ? 'check_box' : 'check_box_outline_blank'}>
+                                <h5>Here's some info on {item.city}!</h5>
+                                {item.visited ? `You have visited ${item.city}!` : `You have not visited ${item.city} yet!`}
+                                <ul>
+                                    <li>Weather:</li>
+                                    <li>Current News:</li>
+                                </ul>
+                                {item.notes.length == 0 ? `You do not currently have any notes.` : item.notes.map(note => {
+                                    return (<p key={note}>{note}</p>)
+                                })}
+                                <NewNote />
+                                <Button waves='light' onClick={() => console.log('remove city')}>Remove City<Icon right>delete</Icon></Button>
+                            </CollapsibleItem>
+                        );
+                    })}
+                </Collapsible>
+            }
+
+
             <NewCity />
         </div>
     );
