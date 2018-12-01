a216f50eb617934dd6000072e509c756f78eaa26
Andrew Harding <dreweharding@gmail.com>
2018-12-01 12:02:53 -0500

    css and minor structure changes | new favicon

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,43 +1,47 @@
 import React from 'react'
+import logo from "./assets/images/bukitlogo.png";
 
 const Home = props => {
 	if (props.user) {
 		return (
-			<div className="Home">
-				<p>Current User:</p>
-				<code>
+			<div className="Home center-align">
+				<p>Be sure to check into your Dashboard to get the latest news for the cities on your Bukit List!</p>
+				{/* <code>
 					{JSON.stringify(props)}
-				</code>
+				</code> */}
 			</div>
 		)
 	} else {
 		return (
-<div className = "container">		
-<div className="row">
-    <div className="col s6 offset-s6">
-      <div className="card-panel teal">
-        <span className="white-text">Want to pin future destinations and keep tabs on the area? Bukit! </span>
+			<div className="container">
+				<div className="row valign-wrapper">
+					<div className="col m6 container">
+						<img className="logo" src={logo}></img>
+					</div>
+					<div className="col m6">
+						<div className="card-panel blue">
+							<span className="white-text">Want to pin future destinations and keep tabs on the area? Bukit! </span>
 
-		<br/>
-		<br/>
-		
-		<span className="white-text">Bukit is an organizer for places you’ve been and places you want to go. 
-Taking vacations or traveling can be complicated, there are so many pieces of information you need to keep tabs on for your destination — 
+							<br />
+							<br />
+
+							<span className="white-text">Bukit is an organizer for places you’ve been and places you want to go.
+					Taking vacations or traveling can be complicated, there are so many pieces of information you need to keep tabs on for your destination —
 What is the weather usually like there? What kind of events occur in the area?</span>
 
-<br/>
-<br/>
+							<br />
+							<br />
 
-<span className="white-text">Instead of navigating, bookmarking multiple sites with static information.
- Use Bukit to enter where you want to go, and track your destinations over time! 
+							<span className="white-text">Instead of navigating, bookmarking multiple sites with static information.
+							 Use Bukit to enter where you want to go, and track your destinations over time!
  Add notes for what you want to do while you’re there, and cross them off your Bucket list!</span>
-        
-      </div>
-    </div>
-  </div>
-  </div>
-			
-			
+
+						</div>
+					</div>
+				</div>
+			</div>
+
+
 		)
 	}
 }
--- src/components/List/List.js
+++ src/components/List/List.js
@@ -10,8 +10,6 @@ const List = props => {
 
         return (
             <div>
-                <p>Hi!</p>
-                
                 {props.user.locations.length === 0 ? `You do not currently have any cities on your list.` :
                     <Collapsible popout>
                         {props.user.locations.map(item => {
@@ -27,6 +25,7 @@ const List = props => {
                                         return (<p key={note}>{note}</p>)
                                     })} */}
                                     <NewNote />
+                                    <hr></hr>
                                     <Button waves='light' onClick={() => console.log('remove city')}>Remove City<Icon right>delete</Icon></Button>
                                 </CollapsibleItem>
                             );
